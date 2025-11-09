0406e311adb21c875f589603db0db96615336045
iXiHashed <ixihashed@gmail.com>
2025-11-09 05:03:05 +0300

    Update encryptor.py
    Function of folder encryption was added

--- encryptor.py
+++ encryptor.py
@@ -8,3 +8,10 @@ def encrypt_file(filename, key):
     with open(filename + ".encrypted", "wb") as file:
         file.write(encrypted_data)
     print(f"Файл {filename} зашифрован как {filename}.encrypted")
+
+def encrypt_folder(folder_path, key):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            filepath = os.path.join(root, file)
+            if not filepath.endswith('.encrypted') and not file.startswith('.'):
+                encrypt_file(filepath, key)
